196f5333a94de518a8cead8c005b0f000266c56d
nikit151102 <nikit15110295@gmail.com>
2024-10-31 19:01:57 +0700

    .

--- src/app/components/authentication/form-authorization/form-authorization.component.ts
+++ src/app/components/authentication/form-authorization/form-authorization.component.ts
@@ -54,7 +54,7 @@ export class FormAuthorizationComponent implements OnInit {
       const Data  = {
         UserName: formData.username,
         Hash: '',
-        Email: formData.email,
+        Email: formData.username,
         Password: formData.password, 
       };
 
