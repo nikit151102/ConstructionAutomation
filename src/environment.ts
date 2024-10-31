da1a2d4a3df6ad0ac0a82428b559b0a3ac98ed87
nikit151102 <nikit15110295@gmail.com>
2024-10-31 20:03:37 +0700

    edit apiUrl

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    // apiUrl: 'https://vm-2ea5fe48.na4u.ru/api',
-    apiUrl: 'http://localhost:5182',
+    apiUrl: 'https://vm-2ea5fe48.na4u.ru/api',
+    // apiUrl: 'http://localhost:5182',
     userNameBot: 'Uteam_top_bot'
 };
 
