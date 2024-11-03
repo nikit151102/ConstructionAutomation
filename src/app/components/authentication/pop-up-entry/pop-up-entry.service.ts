7a4539b6315526dcf0cbf555ccc5c6929eba0b1d
nikit151102 <nikit15110295@gmail.com>
2024-11-03 23:10:24 +0700

    подправка сервиса авторизации тг

--- src/app/components/authentication/pop-up-entry/pop-up-entry.service.ts
+++ src/app/components/authentication/pop-up-entry/pop-up-entry.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environment';
+import { environment } from '../../../../environment';
 
 @Injectable({
   providedIn: 'root'
