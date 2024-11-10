07f6d8190fafdfba70f68890b9de7c30fdd10656
nikit151102 <nikit15110295@gmail.com>
2024-11-11 00:14:28 +0700

    add FormData

--- src/app/pages/personal-account/documents/comparative-statement/comparative-statement.service.ts
+++ src/app/pages/personal-account/documents/comparative-statement/comparative-statement.service.ts
@@ -2,16 +2,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../../environment';
-import { SelectedFiles } from '../../../../interfaces/files';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComparativeStatementService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  uploadFiles(files: SelectedFiles): Observable<any> {
+  uploadFiles(files: FormData): Observable<any> {
     return this.http.post(environment.apiUrl, files, {
       headers: new HttpHeaders({
         'Accept': 'application/json'
