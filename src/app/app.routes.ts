0cc885a05b53504d85c47f75ffd33c23133fd19f
nikit151102 <nikit15110295@gmail.com>
2024-12-15 21:51:04 +0700

    edit routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { AuthGuard } from './pages/personal-account/auth.guard';
 
 export const routes: Routes = [
     {
+        path: '',  loadChildren: () => import('./pages/login-signup/login-signup.module').then(m => m.LoginSignupModule)
+    },
+    {
         path: 'login',  loadChildren: () => import('./pages/login-signup/login-signup.module').then(m => m.LoginSignupModule)
     },
     {
         // canActivate: [AuthGuard]
-        path: ':id',  loadChildren: () => import('./pages/personal-account/personal-account.module').then(m => m.PersonalAccountModule),
+        path: ':id',  loadChildren: () => import('./pages/personal-account/personal-account.module').then(m => m.PersonalAccountModule), canActivate: [AuthGuard]
     },
 
 ];
