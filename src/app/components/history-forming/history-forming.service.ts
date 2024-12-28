16dbbea25e37614520ff2bfe021292bdcb2927e1
nikit151102 <nikit15110295@gmail.com>
2024-12-28 07:39:37 +0700

    fix forming history

---
+++ src/app/components/history-forming/history-forming.service.ts
@@ -0,0 +1,111 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '../../../environment';
+import { dataDocs } from '../../interfaces/files';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistoryFormingService {
+
+  constructor(private http: HttpClient) { }
+
+  selectpdf: any;
+  selectExcel: any;
+  visiblePdf: boolean = false;
+
+  private selectExcelIdSubject = new BehaviorSubject<string>('');
+  selectExcelIdState$ = this.selectExcelIdSubject.asObservable();
+
+  private selectpdfIdSubject = new BehaviorSubject<string>('');
+  selectpdfIdState$ = this.selectpdfIdSubject.asObservable();
+
+
+  updateSelectExcelId(id: string): void {
+    this.selectExcelIdSubject.next(id);
+  }
+
+  updateSelectPdfId(id: string): void {
+    this.selectpdfIdSubject.next(id);
+  }
+
+  private historyDocsSubject = new BehaviorSubject<dataDocs[]>([]);
+  historyDocsState$ = this.historyDocsSubject.asObservable();
+
+  setHistoryDocsValue(doc: dataDocs): void {
+    const currentDocs = this.historyDocsSubject.value || [];
+    this.historyDocsSubject.next([...currentDocs, doc]);
+  }
+
+  getHistoryDocsValue(): any {
+    return this.historyDocsSubject.getValue();
+  }
+
+  loadHistoryDocs(docs: dataDocs[]): void {
+    if (Array.isArray(docs)) {
+      this.historyDocsSubject.next(docs);
+    }
+  }
+
+
+
+
+// Эндпоинт для получения всех объектов очереди
+
+// response
+  //   {
+  //   status
+  //   message
+  //   data[ массив с объектами очереди
+  //  {
+  // id: string,
+  // statusCode: number,
+  // FileName: string,
+  // FileSize: number,
+  // documentType: number, 
+  // InitDate: Date,
+  // DocumentPdfId: string,
+  // DocumentXlsxId: string
+  // }
+  // ] 
+  // }
+
+  getHistoryForming(): Observable<any> {
+    const token = localStorage.getItem('YXV0aFRva2Vu');
+    return this.http.get(`${environment.apiUrl}/api/Profile/HistoryDocumentGenerate`, {
+
+      headers: new HttpHeaders({
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      })
+    });
+  }
+
+
+
+// Эндпоинт для оплаты 
+
+// response
+  // {
+  // status
+  // message
+  // file файл пдф для предпросмотр без вложенных объектов файлов
+  // balance number 
+  // resultXlsxId string
+  // resultPdfId string
+  // }
+
+  makeTransaction(id: string): Observable<any> {
+    const token = localStorage.getItem('YXV0aFRva2Vu');
+    return this.http.put(`${environment.apiUrl}/api/Profile/ConfirmDocumentGenerate`,{
+       "documentInstanceId": id
+    }, {
+      headers: new HttpHeaders({
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      })
+    });
+  }
+
+}
