fd3535d166c0b1b23d482a1bce7c49072095292e
nikit151102 <nikit15110295@gmail.com>
2025-01-02 22:05:42 +0700

    fix

--- src/app/error-handler.service.ts
+++ src/app/error-handler.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, ErrorHandler } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CurrentUserService } from './services/current-user.service';
+import { TokenService } from './services/token.service';
+import { Router } from '@angular/router';
 
 export interface ErrorResponse {
     errorMessage: string;
@@ -13,28 +16,43 @@ export interface ErrorResponse {
     providedIn: 'root',
 })
 export class GlobalErrorHandlerService implements ErrorHandler {
-    private apiUrl = 'https://vm-c86b4df8.na4u.ru/logs/log-error/angular';
+    private apiUrl = 'https://rebuildpro.ru/logs/log-error/angular';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient,
+        private currentUserService: CurrentUserService,
+        private tokenService: TokenService,
+        private router: Router
+    ) { }
 
     handleError(error: any): void {
 
-        const errorResponse: ErrorResponse = {
-            errorMessage: error.message || 'Unknown Error',
-            details: error.stack || 'No details',
-            url: window.location.href,
-            username: '',
-            timestamp: new Date().toISOString(),
-        };
+        if (error.status === 401) {
+            this.handleUnauthorizedError();
+            return;
+        } else {
+            const errorResponse: ErrorResponse = {
+                errorMessage: error.message || 'Unknown Error',
+                details: error.stack || 'No details',
+                url: window.location.href,
+                username: '',
+                timestamp: new Date().toISOString(),
+            };
 
-        this.sendErrorToServer(errorResponse).subscribe({
-            next: (response) => {
-                console.log('Error logged to server:', response);
-            },
-            error: (err) => {
-                console.error('Error while logging to server:', err);
-            },
-        });
+            this.sendErrorToServer(errorResponse).subscribe({
+                next: (response) => {
+                    console.log('Error logged to server:', response);
+                },
+                error: (err) => {
+                    console.error('Error while logging to server:', err);
+                },
+            });
+        }
+    }
+
+    private handleUnauthorizedError() {
+        this.currentUserService.removeUser();
+        this.tokenService.clearToken();
+        this.router.navigate(['/login'], { state: { isSessionExpired: true } });
     }
 
     sendErrorToServer(error: ErrorResponse) {
--- src/app/pages/login-signup/login-signup.component.ts
+++ src/app/pages/login-signup/login-signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthenticationComponent } from '../../components/authentication/authentication.component';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-login-signup',
@@ -9,5 +10,19 @@ import { AuthenticationComponent } from '../../components/authentication/authent
   styleUrl: './login-signup.component.scss'
 })
 export class LoginSignupComponent {
+
+  isSessionExpired: boolean = false;
+
+  constructor(private toastService: ToastService) { }
+
+  ngOnInit(): void {
+    const navigationState = window.history.state;
+    this.isSessionExpired = navigationState.isSessionExpired || false;
+
+    if (this.isSessionExpired) {
+      this.toastService.showWarn('Сессия истекла', 'Пожалуйста, войдите в систему заново.'
+      );
+    }
+  }
 
 }
