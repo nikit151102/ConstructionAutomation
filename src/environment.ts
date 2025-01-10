891249abf2bbdef1f438248ae02d004413e34842
nikit151102 <nikit15110295@gmail.com>
2025-01-10 21:51:08 +0700

    edit apiUrl

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl: 'https://rebuildpro.ru:5001',
-    //apiUrl: 'https://mirshashlykoff.online:5001',
+    //apiUrl: 'https://rebuildpro.ru:5001',
+    apiUrl: 'https://mirshashlykoff.online:5001',
     // apiUrl: 'http://vm-c86b4df8.na4u.ru:5000',
     // apiUrl: 'http://localhost:5182',
     userNameBot: 'etgrrisTESTING_bot'
