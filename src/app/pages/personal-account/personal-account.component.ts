51100b952a8ce5a0dc79c9e0f1c41f0238e8141e
nikit151102 <nikit15110295@gmail.com>
2025-01-16 21:18:32 +0700

    .

--- src/app/pages/personal-account/personal-account.component.ts
+++ src/app/pages/personal-account/personal-account.component.ts
@@ -140,14 +140,13 @@ export class PersonalAccountComponent implements OnInit, OnDestroy {
 
 
   // Функция для создания платежа через ЮKassa API
-  async createPayment() {
+   async createPayment() {
     const url = 'https://api.yookassa.ru/v3/payments';
-    const shopId = '1015227';  // Идентификатор магазина ЮKassa
-    //const secretKey = 'live_A0z7snNh-Wu2vSg0T33gXFz_blH-4cVmocnX3_VmZFo'; // Секретный ключ магазина
-    const secretKey = 'test_mTsXdhSifwi6cApEwep6R0hMRMOHqWcGaWv3CrDSVis'; // Секретный ключ магазина
+    const shopId = '1011761';  // Идентификатор магазина ЮKassa
+    const secretKey = 'live_A0z7snNh-Wu2vSg0T33gXFz_blH-4cVmocnX3_VmZFo'; // Секретный ключ магазина
+    //const secretKey = 'test_mTsXdhSifwi6cApEwep6R0hMRMOHqWcGaWv3CrDSVis'; // Секретный ключ магазина
     
     
-
     const paymentData = {
       amount: {
         value: '2.00', // Сумма платежа
@@ -166,7 +165,6 @@ export class PersonalAccountComponent implements OnInit, OnDestroy {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Basic ${btoa(shopId + ':' + secretKey)}`, // Аутентификация
-
         },
         body: JSON.stringify(paymentData),
       });
