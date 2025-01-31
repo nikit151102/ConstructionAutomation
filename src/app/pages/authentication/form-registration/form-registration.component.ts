e1376a61c857f7996aeb929063c404704291793d
nikit151102 <nikit15110295@gmail.com>
2025-02-01 02:43:23 +0700

    edit signUn

--- src/app/pages/authentication/form-registration/form-registration.component.ts
+++ src/app/pages/authentication/form-registration/form-registration.component.ts
@@ -127,8 +127,8 @@ export class FormRegistrationComponent {
         (response) => {
           this.currentUserService.saveUser(response.data);
           this.tokenService.setToken(response.data.token);
-          this.router.navigate([`/${response.data.id}`]);
-          // this.popUpConfirmEmailService.showPopup();
+          //this.router.navigate([`/${response.data.id}`]);
+          this.popUpConfirmEmailService.showPopup();
           const dataStage = {
             userName: `${response.data.lastName ?? ''} ${response.data.firstName ?? ''}`.trim(),
             email: response.data.email
